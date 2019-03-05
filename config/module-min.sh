5724c5ae1372ff916608366e6c09520b00a50c44
MaxF <FilenkovMaxim@gmail.com>
2019-03-05 12:33:54 +0400

    dubs configuration

--- config/module-min.sh
+++ config/module-min.sh
@@ -2,25 +2,24 @@
 
 #--------------------
 # Standard options:
-export COMMON_FF_CFG_FLAGS=
-
-export COMMON_FF_CFG_FLAGS="$COMMON_FF_CFG_FLAGS --disable-all"
-
-export COMMON_FF_CFG_FLAGS="$COMMON_FF_CFG_FLAGS --enable-avcodec"
-export COMMON_FF_CFG_FLAGS="$COMMON_FF_CFG_FLAGS --enable-avformat"
-export COMMON_FF_CFG_FLAGS="$COMMON_FF_CFG_FLAGS --enable-avutil"
-export COMMON_FF_CFG_FLAGS="$COMMON_FF_CFG_FLAGS --enable-swresample"
-export COMMON_FF_CFG_FLAGS="$COMMON_FF_CFG_FLAGS --enable-swscale"
-export COMMON_FF_CFG_FLAGS="$COMMON_FF_CFG_FLAGS --enable-avfilter"
-export COMMON_FF_CFG_FLAGS="$COMMON_FF_CFG_FLAGS --enable-network"
-
-export COMMON_FF_CFG_FLAGS="$COMMON_FF_CFG_FLAGS --enable-decoder=aac"
-export COMMON_FF_CFG_FLAGS="$COMMON_FF_CFG_FLAGS --enable-decoder=h264"
-
-export COMMON_FF_CFG_FLAGS="$COMMON_FF_CFG_FLAGS --enable-muxer=mp4"
-
-export COMMON_FF_CFG_FLAGS="$COMMON_FF_CFG_FLAGS --enable-demuxer=aac"
-export COMMON_FF_CFG_FLAGS="$COMMON_FF_CFG_FLAGS --enable-demuxer=h264"
-
-export COMMON_FF_CFG_FLAGS="$COMMON_FF_CFG_FLAGS --enable-parser=aac"
-export COMMON_FF_CFG_FLAGS="$COMMON_FF_CFG_FLAGS --enable-parser=h264"
+export COMMON_FF_CFG_FLAGS=" --disable-all \
+ --disable-everything \
+ --disable-asm \
+ --disable-iconv \
+ --disable-zlib \
+ --disable-videotoolbox \
+ --disable-audiotoolbox \
+ --disable-doc \
+ --disable-programs \
+ --disable-avdevice \
+ --disable-debug \
+ --disable-static \
+ --enable-shared \
+ --enable-small \
+ --enable-runtime-cpudetect \
+ --enable-avcodec --enable-avformat --enable-avutil --enable-swresample --enable-swscale --enable-avfilter \
+ --enable-decoder=aac \
+ --enable-demuxer=mov \
+ --enable-cross-compile \
+ --enable-protocol=file \
+ --enable-pic "
